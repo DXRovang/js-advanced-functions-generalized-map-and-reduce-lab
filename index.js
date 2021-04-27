dd907cccdc8305ecb64bece6e4e44dbbb8fc4218
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-27 16:10:29 -0400

    commit tree 2d5018b874072d438ab86b1a255ae9bbf5aafa55

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+
+
+//src is an array, //callback is a function that's already been defined
+function map(src, callback){
+  // r is the array which will be returned
+  const r = []
+
+  for (let i = 0; i < src.length; i++ ){
+
+    // theElement is the piece of the array that the callback will work on
+    const theElement = src[i];
+    // pushes the result of the callback into r
+    r.push(callback(theElement))
+  }
+  return r
+}
+
+function reduce(src="", callback){
+  
+}
